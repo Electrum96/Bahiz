1622e7b202d0e57b9ce0cc2ac8acf7a9c2327d52
electrum <kachalina.kate@mail.ru>
2023-11-14 19:33:20 +0300

    14-11-2023:19:33

--- src/pages/successful/Successful.jsx
+++ src/pages/successful/Successful.jsx
@@ -1,15 +1,29 @@
 import {Text, View, Image} from 'react-native';
 
 import useStyleSuccessful from './useStyleSuccessful';
+import Layout from '../../components/layout/Layout';
+import FooterButton from '../../components/footerButton/FooterButton';
 
 const Successful = () => {
+  const styles = useStyleSuccessful();
+  return (
+    <Layout>
+      <View style={styles.page}>
+        <Image
+          style={styles.image}
+          source={require('../../image/background/suc.png')}
+          resizeMode="contain"
+        />
+        <Text>Your order has been successfully placed</Text>
+        <Image
+          style={styles.code}
+          source={require('../../image/icon/square.png')}
+        />
 
-const styles = useStyleSuccessful();
-    return (
-        <View>
-            <Text>Successful</Text>
-        </View>
-    )
-}
+        <FooterButton title={'Go back to home'} />
+      </View>
+    </Layout>
+  );
+};
 
 export default Successful;
--- src/pages/successful/useStyleSuccessful.js
+++ src/pages/successful/useStyleSuccessful.js
@@ -2,7 +2,16 @@ import {StyleSheet} from 'react-native';
 
 function useStyleSuccessful () {
     const styles = StyleSheet.create({
-        
+        page:{
+            height: '90%',
+        },
+        image: {
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center'
+
+        },
     });
 
     return styles;
