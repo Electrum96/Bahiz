c4a5035862cfcec7fc3f6f974a29ff3ae251aeaf
electrum <kachalina.kate@mail.ru>
2023-11-18 10:21:48 +0300

    18-11-2023:10:21

--- src/pages/broning/Broning.jsx
+++ src/pages/broning/Broning.jsx
@@ -1,14 +1,33 @@
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, TextInput} from 'react-native';
 
 import useStyleBroning from './useStyleBroning';
+import Layout from '../../components/layout/Layout';
+import FooterButton from '../../components/footerButton/FooterButton';
 
-const Broning = () => {
+const Broning = ({navigation}) => {
 
 const styles = useStyleBroning();
     return (
-        <View>
-            <Text>Broning</Text>
-        </View>
+        <Layout navigation={navigation}>
+            <View style={styles.pageWrap}>
+
+                <View style={styles.formWrap}>
+                <Text style={styles.title}>Бронь</Text>
+                <Text style={styles.desc}>Email Address</Text>
+                <TextInput style={styles.input}/>
+
+                <Text style={styles.desc}>Phone</Text>
+                <TextInput style={styles.input}/>
+
+                <Text style={styles.desc}>Table</Text>
+                <TextInput style={styles.input}/>
+                </View>
+
+                <Text style={styles.text}>Welcome friend, enter your details so lets get started  food.</Text>
+                <FooterButton  title={'GO MAIN'} onPress={() => navigation.push('Thanks')}/>
+                
+            </View>
+        </Layout>
     )
 }
 
--- src/pages/broning/useStyleBroning.js
+++ src/pages/broning/useStyleBroning.js
@@ -1,8 +1,35 @@
 import {StyleSheet} from 'react-native';
+import { COLORS } from '../../styles/variables';
 
 function useStyleBroning () {
     const styles = StyleSheet.create({
-        
+        pageWrap: {
+            height:'100%',
+            justifyContent: 'space-around',
+         
+        },
+        formWrap:{
+            gap:5
+        },
+        input:{
+            borderWidth: 1,
+            borderColor: COLORS.accentBg,
+            borderRadius: 10
+        },
+        title:{
+            fontSize: 25,
+            color: COLORS.dark,
+            fontWeight:'600',
+            paddingBottom: 10
+        },
+        desc:{
+            color: COLORS.dark
+        },
+        text:{
+            color:COLORS.dark,
+            marginBottom: -20
+        },
+      
     });
 
     return styles;
