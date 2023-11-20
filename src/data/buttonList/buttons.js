71de9f579ec5b5c72a1940a81833a3d99d875e4a
electrum <kachalina.kate@mail.ru>
2023-11-20 18:39:24 +0300

    20-11-2023:18:39

--- src/data/buttonList/buttons.js
+++ src/data/buttonList/buttons.js
@@ -9,20 +9,20 @@ const buttons = [
   
     {
       title: 'Almuerzo',
-      image: require('../../image/Lunch/Kalmar.jpeg'),
+      image: require('../../image/Lunch/Kalmar.png'),
       classTab: 'Lunch',
       id: 2,
     },
   
     {
       title: 'Cena',
-      image: require('../../image/Dinner/Bif.jpeg'),
+      image: require('../../image/Dinner/Bif.png'),
       classTab: 'Dinner',
       id: 3,
     },
     {
       title: 'Todo',
-      image: require('../../image/Lunch/Yellow.jpeg'),
+      image: require('../../image/Lunch/Yellow.png'),
       classTab: 'All',
       id: 4,
     },
