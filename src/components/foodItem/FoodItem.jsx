d5757aba014c5368c6ea4fc679ce48baeae56570
electrum <kachalina.kate@mail.ru>
2023-11-20 12:24:19 +0300

    20-11-2023:12:24

--- src/components/foodItem/FoodItem.jsx
+++ src/components/foodItem/FoodItem.jsx
@@ -8,11 +8,11 @@ const FoodItem = ({navigation,image, title, price, desc, id}) => {
   return (
     <TouchableOpacity onPress={() => navigation.push('FoodCard', {id})}>
     <View style={styles.itemWrap}>
-      <Image style={styles.image} source={image} />
+      <Image style={styles.image} source={image} resizeMode='cover'/>
       <View style={styles.infoWrap}>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.descr}>{desc}</Text>
-      <Text style={styles.price}>{price}</Text>
+      <Text style={styles.price}>{price} € </Text>
       </View>
     </View>
     </TouchableOpacity>
--- src/components/foodItem/useStyleFoodItem.js
+++ src/components/foodItem/useStyleFoodItem.js
@@ -5,8 +5,7 @@ function useStyleFoodItem() {
   const styles = StyleSheet.create({
     itemWrap: {
       width: 160,
-      height: 200,
-      backgroundColor: COLORS.accentBg,
+      height: 240,
       borderRadius: 10,
       overflow: 'hidden',
       shadowColor: 'rgba(0, 0, 0, 0.6)',
@@ -16,15 +15,32 @@ function useStyleFoodItem() {
       shadowOffset : { width: 1, height: 13},
     },
     image: {
-      alignSelf: 'center',
       flex: 1,
-      width: 160,
-      height: 300,
+      width:"100%",
+      height:"100%",
+      alignContent: 'center'
     },
     infoWrap: {
+      height: '43%',
+      width: '100%',
       alignItems: 'flex-start',
       backgroundColor: COLORS.accent,
+      paddingHorizontal: 10,
+      paddingTop: 10,
+      justifyContent: 'space-around'
     },
+    title: {
+      fontWeight: '700',
+      fontSize: 13
+    },
+    descr: {
+     fontSize: 10
+    },
+    price :{
+      fontWeight: '600',
+      fontSize: 15,
+      marginBottom: 10
+    }
   });
 
   return styles;
