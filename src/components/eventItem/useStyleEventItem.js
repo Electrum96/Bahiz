eda6a2f43a50da5d41a4d424d96bcd852fb9ceed
electrum <kachalina.kate@mail.ru>
2023-11-20 17:46:58 +0300

    20-11-2023:17:46

--- src/components/eventItem/useStyleEventItem.js
+++ src/components/eventItem/useStyleEventItem.js
@@ -1,39 +1,36 @@
 import {StyleSheet} from 'react-native';
-import { COLORS } from '../../styles/variables';
+import {COLORS} from '../../styles/variables';
 
-function useStyleEventItem () {
-    const styles = StyleSheet.create({
-        wrapItem:{
-            width: '45%',
-            height: 400,
-            gap: 3,
-           overflow: 'hidden'
-            
-            
-        },
-        image:{
-            width:'100%',
-            height: '50%',
-            borderRadius: 10
-        },
-        title:{
-            fontSize: 14,
-            fontWeight: '600',
-            color: COLORS.dark
-        },
-        descWrap:{
-         height: 200,
-         width: 150
-         
-        },
-        desc:{
-            fontSize: 8,
-            color: COLORS.dark
-        }
+function useStyleEventItem() {
+  const styles = StyleSheet.create({
+    wrapItem: {
+      width: 150,
+      minHeight: 300,
+      gap: 3,
+      overflow: 'hidden',
+    },
+    image: {
+      width: '100%',
+      height: 150,
+      borderRadius: 10,
+    },
+    title: {
+      fontSize: 14,
+      fontWeight: '600',
+      color: COLORS.dark,
+    },
+    descWrap: {
+      height: 200,
+    
+    },
+    desc: {
+      fontSize: 8,
+      color: COLORS.dark,
+      flex: 1
+    },
+  });
 
-    });
-
-    return styles;
+  return styles;
 }
 
 export default useStyleEventItem;
